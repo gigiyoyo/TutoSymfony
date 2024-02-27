c87f1c37432eafed33a685666d1f9fa3d9f94f11
gigiyoyo <yoyo24gigi2@gmail.com>
2024-02-27 12:16:06 +0000

    Affichage des recettes.

---
+++ Cuisinova/src/Controller/RecipeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recipes', name: 'recipe_index')]
+    public function index(RecipeRepository $recipeRepository): Response
+    {
+        $recipes = $recipeRepository->findAll();
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
+    }
+
+    #[Route('/recipes/{slug}-{id}', name: 'recipe_show', requirements:['id'=>'\d+', 'slug'=>'[^/]+'])]
+    public function show(Request $request, RecipeRepository $recipeRepository, string $slug, int $id): Response
+    {
+        $recipe = $recipeRepository->find($id);
+        if ($slug !== $recipe->getSlug()) {
+            return $this->redirectToRoute('recipe_show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe
+        ]);
+    }
+}
