c65a429b6873809a6007773ae0b660e887b5bf94
gigiyoyo <yoyo24gigi2@gmail.com>
2024-02-28 14:53:37 +0000

    autoSlug si le champ est vide.

--- Cuisinova/src/Form/RecipeType.php
+++ Cuisinova/src/Form/RecipeType.php
@@ -3,10 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Recipe;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RecipeType extends AbstractType
 {
@@ -18,7 +21,18 @@ class RecipeType extends AbstractType
             ->add('content')
             ->add('duration')
             ->add('save', SubmitType::class)
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...)) # Collable
         ;
+    }
+
+    public function autoSlug(PreSubmitEvent $event): void {
+        $data = $event->getData();
+        
+        if(trim($data['slug']) === ""){         # or if(empty($data['slug']))
+            $slugger = new AsciiSlugger();
+            $data['slug'] = $slugger->slug($data['title'])->lower();
+            $event->setData($data);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
